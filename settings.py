f91f7a60399aecab6b6eb8bef368f1f9e8c347e0
Jessica Spencer <jessica.spencer@copperrivercs.com>
2019-09-13 19:06:52 -0400

    Created settings and added all alien_game settings

---
+++ settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 18:58:55 2019
+
+@author: JessicaSpencer
+"""
+
+class Settings:
+    """Creating a class to store all alien_game settings"""
+    
+    def __init__(self):
+        """Creating game settings"""
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
